d3f773717be977e962310ed8f237c2276e7a2f65
Cédric L. Charlier <cedric.charlier@yucom.be>
2019-12-22 18:01:35 +0100

    Fix duplicate in AssemblyVersion tags

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -8,8 +8,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Syncopq")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0")]
-
 [assembly: InternalsVisibleTo("Syncopq.Testing")]
